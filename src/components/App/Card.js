2ce13adc94b5f45a3d6e873330e025d624d2eee4
Ali Nooner <alinooner@gmail.com>
2022-04-06 12:56:55 -0500

    Run Prettier

--- src/components/App/Card.js
+++ src/components/App/Card.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import './Card.css'
+import React from "react";
+import "./Card.css";
 
-const Card = ({stance, name, obstacle, tutorial, id, deleteTrick}) => {
+const Card = ({ stance, name, obstacle, tutorial, id, deleteTrick }) => {
   return (
-    <div className='card'>
-      <h3>{stance} {name}</h3>
+    <div className="card">
+      <h3>
+        {stance} {name}
+      </h3>
       <p>Obstacle: {obstacle}</p>
       <p>Tutorial: {tutorial}</p>
-      <button onClick= {() => deleteTrick(id)}>x</button>
+      <button onClick={() => deleteTrick(id)}>x</button>
     </div>
-  )
-}
-
-
-
-
+  );
+};
 
 export default Card;
